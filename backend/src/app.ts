248cbce106a321e90be0497dc2257e718926792e
Itanaki <qedbpadual@tip.edu.ph>
2026-08-13 20:53:00 +0800

    - addition of frontend port

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 import quizzesRouter from './routes/quizzes';
 import attemptRouter from './routes/attempt';
 
 const app = express();
 
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 
 app.use('/api/quizzes', quizzesRouter);
