45ac15985f814bc52ba8c58cd9d66c503a8433ec
Itanaki <qedbpadual@tip.edu.ph>
2026-08-14 19:28:43 +0800

    - updated attempt quiz controller because of the addition of zod valdiation

--- backend/src/controllers/attempts.controller.ts
+++ backend/src/controllers/attempts.controller.ts
@@ -1,13 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
+import { attemptSchema } from '../schemas/attempt.schema';
 import { submitAttempt } from '../services/quiz.service';
 
-export async function postAttempt(req: Request, res: Response, next: NextFunction) {
+export async function postAttempt(
+  req: Request, 
+  res: Response, 
+  next: NextFunction
+  ) {
   try {
-    const payload = {
-      userId: (req as any).user?.id, // if auth exists
-      answers: req.body.answers || [],
-    };
-    const result = await submitAttempt(payload);
+    const payload = attemptSchema.parse(req.body);
+
+    const result = await submitAttempt({
+      userId: (req as any).user?.id,
+      answers: payload.answers,
+    });
+    
     res.status(201).json(result);
   } catch (err) {
     next(err);
